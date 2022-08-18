e59967287996734b2f7e288176a62eae068a8184
yis19 <65199911+yisus2099@users.noreply.github.com>
2022-08-18 09:46:33 -0600

    Add files via upload

---
+++ EVA1-1-STACK/src/eva1/pkg1/stack/EVA11STACK.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1.pkg1.stack;
+
+/**
+ *
+ * @author Jesùs Alejandro Torres Ramìrez
+ */
+public class EVA11STACK {
+
+    /**
+     * @param args the command line arguments 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        
+     A();
+    }
+
+    public static void A() {
+        B();
+    }
+
+    public static void B() {
+        int i = 1;
+        double d = 10.0;
+        
+
+    }
+
+}
